8530a2f2ac6fe87d8d0cd705d0cb5d094ad6ce13
asijit123 <64984886+asijit123@users.noreply.github.com>
2020-09-07 14:26:53 +0530

    Add files via upload

---
+++ tictactoe/src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            Scene scene = new Scene(root,300,320);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
